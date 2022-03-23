6f38f265f0ea25d5c08a2884ed77a9481ba76bbe
123falksdfj <changnan666@gmail.com>
2022-03-23 18:40:58 +0800

    testing

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,51 @@
+import React from 'react';
 import { AntdForm } from '.';
+import { FormItemProps } from './types';
 
-describe('ExampleComponent', () => {
+import { unmountComponentAtNode, render } from 'react-dom';
+import { act } from '@testing-library/react';
+
+let container: HTMLDivElement | null = null;
+
+beforeEach(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn()
+    }))
+  });
+
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  container && unmountComponentAtNode(container);
+  container && container.remove();
+  container = null;
+});
+
+describe('AntdForm', () => {
   it('is truthy', () => {
     expect(AntdForm).toBeTruthy();
+  });
+
+  it('render type', () => {
+    const props: FormItemProps[] = [];
+    const onFinished = () => {};
+
+    act(() => {
+      render(<AntdForm formItems={props} onFinished={onFinished} />, container);
+    });
+
+    console.log(container?.innerHTML);
   });
 });
