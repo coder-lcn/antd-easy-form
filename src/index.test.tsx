b0c79b628a19ed8c26526709d22e602559ea421d
123falksdfj <changnan666@gmail.com>
2022-03-23 18:34:04 +0800

    update

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { AntdForm } from '.';
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+    expect(AntdForm).toBeTruthy();
+  });
+});
